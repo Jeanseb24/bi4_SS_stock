e1188e4c88fb5d40940124bc905bd1ef5a1f1982
jeanseb24 <jeanseb24@live.be>
2026-09-06 12:23:33 +0200

    clé_accès_admin_dev

--- partials/foot.php
+++ partials/foot.php
@@ -1,5 +1,16 @@
 <footer class="bg-light text-center">
     &copy; Copyright 2026
 </footer>
+
+<!-- Bouton temporaire Dev (À supprimer avant la mise en production) -->
+<div class="text-center my-3">
+    <a href="admin/index.php" title="Accès rapide Admin">
+        <i class="bi bi-key-fill fs-4"></i>
+    </a>
+    <div class="small text-danger fw-bold mt-1">
+        <i class="bi bi-exclamation-triangle-fill me-1"></i>à supprimer après dev
+    </div>
+</div>
+
 </body>
 </html>
